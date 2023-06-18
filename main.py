99de62142fa8ebdf68010fef91f03d9ec91c7312
aZticot <osko.mujic49@gmail.com>
2023-06-18 15:57:51 +0200

    1st commit

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import subprocess
 import xml.etree.ElementTree as ET
 import tkinter as tk
-from tkinter import filedialog, messagebox
+from tkinter import *
+from tkinter import filedialog, messagebox, ttk
 import getpass
 import json
 import xml.dom.minidom as minidom
 from openpyxl import Workbook, load_workbook
+
+
 def extract_cpu_info(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
@@ -85,25 +88,51 @@ def extract_motherboard_info(xml_file):
     return motherboard_info
 
 
-def extract_memory_info(xml_file):
+def extract_memory_size_info(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
-    memory_info = {
-        'Drive Capacity': [],
-        'Media Rotation Rate': []
-    }
-
+    memory_size_info = []
     for subnode in root.iter('SubNode'):
         for property_node in subnode.iter('Property'):
             entry = property_node.find('Entry').text
             description = property_node.find('Description').text
-            if entry == 'Drive Capacity':
-                memory_info['Drive Capacity'].append(description)
-            elif entry == 'Media Rotation Rate':
-                memory_info['Media Rotation Rate'].append(description)
+            if entry == 'Drive Capacity' and description not in memory_size_info:
+                memory_size_info.append(description)
 
-    return memory_info
+    return memory_size_info
+
+
+
+
+def extract_username_info(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    username_info = {}
+    for memory_node in root.iter('COMPUTER'):
+        for property_node in memory_node.iter('Property'):
+            entry = property_node.find('Entry').text
+            description = property_node.find('Description').text
+            if entry == 'Current User Name':
+                username_info['Current User Name'] = description
+
+    return username_info
+
+
+def extract_memory_type_info(xml_file):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+
+    memory_type_info = []
+    for subnode in root.iter('SubNode'):
+        for property_node in subnode.iter('Property'):
+            entry = property_node.find('Entry').text
+            description = property_node.find('Description').text
+            if entry == 'Media Rotation Rate' and description not in memory_type_info:
+                memory_type_info.append(description)
+
+    return memory_type_info
 
 
 def get_domain_info():
@@ -186,6 +215,7 @@ def delete_custom_field(fields, field_frame):
         fields.remove(field_frame)
         update_template_file(fields, field_name)
 
+
 def update_template_file(fields, deleted_field_name):
     template = []
     for field_frame in fields:
@@ -210,26 +240,25 @@ def select_xml_file():
         monitor_info = extract_monitor_info(file_path)
         ram_info = extract_ram_info(file_path)
         motherboard_info = extract_motherboard_info(file_path)
-        memory_info = extract_memory_info(file_path)
+        memory_size_info = extract_memory_size_info(file_path)
+        memory_type_info = extract_memory_type_info(file_path)
         domain_info = get_domain_info()
+        username_info = extract_username_info(file_path)
 
         data = {
             'Processor': cpu_info.get('CPU Brand Name'),
             'Graphics card': gpu_info.get('Video Chipset'),
-            'Monitors': ', '.join(monitor_info),
+            'Monitors': ': '.join(monitor_info),
             'RAM': ram_info.get('Total Memory Size'),
             'Motherboard': motherboard_info.get('Motherboard Model'),
             'Domain': domain_info,
-            'Memory 1 capacity': memory_info.get('Drive Capacity', [''])[0],
-            'Memory 1 type': memory_info.get('Media Rotation Rate', [''])[0],
-            'Memory 2 capacity': memory_info.get('Drive Capacity', [''])[1],
-            'Memory 2 type': memory_info.get('Media Rotation Rate', [''])[1],
-            'Username': getpass.getuser()
+            'Memory capacity': ': '.join(memory_size_info),
+            'Memory type': ': '.join(memory_type_info),
+            'Username': username_info.get('Current User Name')
         }
 
         create_info_window(cpu_info, gpu_info, monitor_info, ram_info, motherboard_info, domain_info,
-                           memory_info, data, file_path)
-
+                           memory_size_info, memory_type_info, username_info, data, file_path)
 
 
 def save(xml_file, data, custom_fields):
@@ -272,7 +301,7 @@ def save(xml_file, data, custom_fields):
     if not data_exists(excel_file):
         create_excel_file(excel_file)
 
-    save_data_to_excel(excel_file, data)
+    save_data_to_excel(excel_file, data, custom_fields)
 
 
 def data_exists(excel_file):
@@ -282,21 +311,45 @@ def data_exists(excel_file):
     except FileNotFoundError:
         return False
 
+
 def create_excel_file(excel_file):
     workbook = Workbook()
     sheet = workbook.active
 
     headers = [
         'Processor', 'Graphics card', 'Monitors', 'RAM', 'Motherboard', 'Domain',
-        'Memory 1 capacity', 'Memory 1 type', 'Memory 2 capacity', 'Memory 2 type', 'Username'
+        'Memory capacity', 'Memory type', 'Username'
     ]
 
     sheet.append(headers)
     workbook.save(excel_file)
 
-def save_data_to_excel(excel_file, data):
+
+# Function to load the current_row value from a JSON file
+def load_current_row():
+    try:
+        with open("current_row.json", "r") as file:
+            data = json.load(file)
+            return data["current_row"]
+    except FileNotFoundError:
+        return 0
+
+
+# Function to save the current_row value to a JSON file
+def save_current_row(current_row):
+    data = {"current_row": current_row}
+    with open("current_row.json", "w") as file:
+        json.dump(data, file)
+
+
+def save_data_to_excel(excel_file, data, custom_fields):
     workbook = load_workbook(excel_file)
     sheet = workbook.active
+
+    # Find the next available row
+    last_row = sheet.max_row
+    current_row = last_row + 1
+    save_current_row(current_row)
 
     row_data = [
         data.get('Processor'),
@@ -305,21 +358,53 @@ def save_data_to_excel(excel_file, data):
         data.get('RAM'),
         data.get('Motherboard'),
         data.get('Domain'),
-        data.get('Memory 1 capacity', ''),
-        data.get('Memory 1 type', ''),
-        data.get('Memory 2 capacity', ''),
-        data.get('Memory 2 type', ''),
+        data.get('Memory capacity'),
+        data.get('Memory type'),
         data.get('Username')
     ]
 
-    sheet.append(row_data)
+    # Get the last column index with data
+    last_column = sheet.max_column
+
+    # Add the custom field labels to the header row if they don't exist
+    for i, field_frame in enumerate(custom_fields):
+        field_label = field_frame.winfo_children()[0]['text'][:-1]
+        header_exists = False
+        for column in range(1, last_column + 1):
+            if sheet.cell(row=1, column=column).value == field_label:
+                header_exists = True
+                break
+        if not header_exists:
+            sheet.cell(row=1, column=last_column + i + 1).value = field_label
+
+    # Update the last column index with data
+    last_column = sheet.max_column
+
+    # Add the data from custom fields
+    for i, field_frame in enumerate(custom_fields):
+        field_label = field_frame.winfo_children()[0]['text'][:-1]
+        field_entry = field_frame.winfo_children()[1]  # Access the Entry widget within the frame
+        description = field_entry.get()
+        column_index = None
+        for column in range(1, last_column + 1):
+            if sheet.cell(row=1, column=column).value == field_label:
+                column_index = column
+                break
+        if column_index is None:
+            column_index = last_column + i + 1
+            sheet.cell(row=1, column=column_index).value = field_label
+        sheet.cell(row=current_row, column=column_index).value = description
+
+    # Add the row data
+    for i, value in enumerate(row_data):
+        sheet.cell(row=current_row, column=i + 1).value = value
+
+    # Save the modified Excel file
     workbook.save(excel_file)
 
 
-
-
-
-def create_info_window(cpu_info, gpu_info, monitor_info, ram_info, motherboard_info, domain_info, memory_info, data, xml_file):
+def create_info_window(cpu_info, gpu_info, monitor_info, ram_info, motherboard_info, domain_info, memory_size_info,
+                       memory_type_info, username_info, data, xml_file):
     window = tk.Toplevel()
     window.title('System Information')
     window.geometry('200x900')
@@ -327,7 +412,6 @@ def create_info_window(cpu_info, gpu_info, monitor_info, ram_info, motherboard_i
 
     # Set the alignment to the left
     window.grid_propagate(False)
-
     # Custom fields
     custom_fields = []
     custom_fields_frame = tk.Frame(window)
@@ -388,33 +472,33 @@ def create_info_window(cpu_info, gpu_info, monitor_info, ram_info, motherboard_i
     domain_entry.pack(anchor='w')
 
     # Memory size
-    memory_sizes = memory_info.get('Drive Capacity', [])[:2]
-    memory_types = memory_info.get('Media Rotation Rate', [])[:2]
+    for i, memory_size in enumerate(memory_size_info):
+        tk.Label(window, text=f'Drive size {i + 1}:').pack(anchor='w')
+        memory_size_var = tk.StringVar()
+        memory_size_var.set(memory_size)
+        memory_size_entry = tk.Entry(window, textvariable=memory_size_var, state='normal', width=30)
+        memory_size_entry.pack(anchor='w')
 
-    for i, (size, mem_type) in enumerate(zip(memory_sizes, memory_types)):
-        tk.Label(window, text=f'Memory {i + 1} capacity:').pack(anchor='w')
-        capacity_var = tk.StringVar()
-        capacity_var.set(size)
-        capacity_entry = tk.Entry(window, textvariable=capacity_var, state='normal', width=30)
-        capacity_entry.pack(anchor='w')
-
-        tk.Label(window, text=f'Memory {i + 1} type:').pack(anchor='w')
-        type_var = tk.StringVar()
-        type_var.set(mem_type)
-        type_entry = tk.Entry(window, textvariable=type_var, state='normal', width=30)
-        type_entry.pack(anchor='w')
+    # Memory type
+    for i, memory_type in enumerate(memory_type_info):
+        tk.Label(window, text=f'Memory type {i + 1}:').pack(anchor='w')
+        memory_type_var = tk.StringVar()
+        memory_type_var.set(memory_type)
+        memory_type_entry = tk.Entry(window, textvariable=memory_type_var, state='normal', width=30)
+        memory_type_entry.pack(anchor='w')
 
     # Username
     tk.Label(window, text='Username:').pack(anchor='w')
     username_var = tk.StringVar()
-    username_var.set(getpass.getuser())
+    username_var.set(username_info.get('Current User Name'))
     username_entry = tk.Entry(window, textvariable=username_var, state='normal', width=30)
     username_entry.pack(anchor='w')
 
     load_template(window, custom_fields)
 
+
 root = tk.Tk()
 root.withdraw()
-
+current_row = load_current_row()
 select_xml_file()
 root.mainloop()
